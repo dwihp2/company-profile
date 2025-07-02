3f554d5f65d8cdebc5617e51531f1c9557e64b14
dwi-enercloud <dwi.putro@enercloud.io>
2025-07-02 23:16:12 +0700

    feat: update configuration files to include sharp dependency and add vercel configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['localhost'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default withPayload(nextConfig);
